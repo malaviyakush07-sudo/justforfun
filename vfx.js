81f8aa611e5a5f96b80f13ca089fc24286945588
malaviyakush07-sudo <malaviyakush07@gmail.com>
2026-04-18 01:51:05 +0530

    Add files via upload

--- vfx.js
+++ vfx.js
@@ -1,3 +1,4 @@
+// HEART CURSOR
 document.addEventListener("mousemove", function(e) {
   let heart = document.createElement("div");
   heart.innerHTML = "❤️";
@@ -8,6 +9,16 @@ document.addEventListener("mousemove", function(e) {
   heart.style.animation = "floatUp 1s linear";
 
   document.body.appendChild(heart);
-
   setTimeout(() => heart.remove(), 1000);
 });
+
+// BACKGROUND PARTICLES
+for (let i = 0; i < 25; i++) {
+  let p = document.createElement("div");
+  p.classList.add("particle");
+
+  p.style.left = Math.random() * window.innerWidth + "px";
+  p.style.animationDuration = (5 + Math.random() * 5) + "s";
+
+  document.body.appendChild(p);
+}

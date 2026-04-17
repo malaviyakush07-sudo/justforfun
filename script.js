ba1b8660dd4743ad363d6c7e4bdf1402956d8946
malaviyakush07-sudo <malaviyakush07@gmail.com>
2026-04-17 21:41:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
+function moveNoButton() {
+  const btn = document.getElementById("noBtn");
+  const x = Math.random() * (window.innerWidth - 120);
+  const y = Math.random() * (window.innerHeight - 80);
 
-function moveRandomEl(elm) {
-  elm.style.position = "absolute";
-  elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
-  elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+  btn.style.position = "absolute";
+  btn.style.left = x + "px";
+  btn.style.top = y + "px";
 }
-
-const moveRandom = document.querySelector("#move-random");
-
-moveRandom.addEventListener("mouseenter", function (e) {
-  moveRandomEl(e.target);
-});
